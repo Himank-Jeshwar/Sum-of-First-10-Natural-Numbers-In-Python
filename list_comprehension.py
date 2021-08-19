d120684d202a4e3f8473ec76477f9ae294bbd3ea
Himank-Jeshwar <technicalhimank@gmail.com>
2021-08-19 14:10:51 +0530

    Sum of First 10 Natural No. (List Comprehension)

---
+++ list_comprehension.py
@@ -0,0 +1,8 @@
+# AUTHOR - HIMANK JESHWAR
+# DATE - 19/08/21
+
+'''PROBLEM :-
+    Find the Sum Of First 10 Natural Numbers Using List Comprehension'''
+    
+l1=[i for i in range(1,11)]  
+print("Sum of First 10 Natural Numbers = ",sum(l1))
